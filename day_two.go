6e6e83efd17ec02089bce9a6dc13c1d7bf9a7fa8
averybos <abostick@indigoag.com>
2023-12-06 11:30:49 -0600

    part 1 answered

--- day_two.go
+++ day_two.go
@@ -3,10 +3,11 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"strconv"
 	"strings"
 )
 
-type GameLimit struct {
+type Game struct {
 	Color  string
 	Amount int
 }
@@ -15,37 +16,71 @@ func parse_each_game() {
 	fileScanner := read_file("day_two_input.txt")
 	fileScanner.Split(bufio.ScanLines)
 
-	var red_limit = GameLimit{
+	var red_limit = Game{
 		Color:  "red",
 		Amount: 12,
 	}
 
-	var green_limit = GameLimit{
+	var green_limit = Game{
 		Color:  "green",
 		Amount: 13,
 	}
 
-	var blue_limit = GameLimit{
+	var blue_limit = Game{
 		Color:  "blue",
 		Amount: 14,
 	}
 
-	fmt.Print(red_limit, green_limit, blue_limit)
+	var allowed_to_play []int
 	for fileScanner.Scan() {
 		text := fileScanner.Text()
 
 		game_id := strings.Split(strings.Split(text, ":")[0], " ")[1]
-		// convert to an int ?
-
-		fmt.Print(game_id, "\n")
+		game_id_number, _ := strconv.Atoi(game_id)
 
 		split_by_semicolon := strings.Split(text, ":")
 		join_back := strings.Join(split_by_semicolon[1:], ", ")
 		games := strings.Split(join_back, ";")
 
+		too_high := false
 		for _, games := range games {
-			fmt.Print(games, "\n")
+
+			// split on commas to get the amount per color
+			each_color := strings.Split(games, ",")
+			for _, color := range each_color {
+				// create a struct for color and amount
+				color_and_amount := strings.Split(color, " ")
+				number, _ := strconv.Atoi(color_and_amount[1])
+
+				var color_struct = Game{}
+				color_struct.Color = color_and_amount[2]
+				color_struct.Amount = number
+
+				// fmt.Print(color_struct.Color, color_struct.Amount)
+				if color_struct.Color == red_limit.Color {
+					if color_struct.Amount > red_limit.Amount {
+						too_high = true
+					}
+				} else if color_struct.Color == green_limit.Color {
+					if color_struct.Amount > green_limit.Amount {
+						too_high = true
+					}
+				} else {
+					if color_struct.Amount > blue_limit.Amount {
+						too_high = true
+					}
+				}
+			}
+		}
+		if too_high == false {
+			allowed_to_play = append(allowed_to_play, game_id_number)
 		}
 
 	}
+	bigNum := 0
+	for _, i := range allowed_to_play {
+		bigNum = bigNum + i
+	}
+	// final answer
+	fmt.Print(bigNum)
 }
